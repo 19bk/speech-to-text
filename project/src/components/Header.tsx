6c2c165dcd37467b8683bc0c1026fec2e434eaae
19bk <bernardkibathi91@gmail.com>
2024-11-12 21:38:14 +0300

    modified:   project/src/components/Header.tsx

--- project/src/components/Header.tsx
+++ project/src/components/Header.tsx
@@ -2,7 +2,7 @@ import React, { useState, useRef } from 'react';
 import { Bell, Search, LogOut, Upload } from 'lucide-react';
 import { useAuthStore } from '../store/authStore';
 
-const DEFAULT_AVATAR = "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80";
+const DEFAULT_AVATAR = "https://unsplash.com/photos/man-wearing-black-crew-neck-t-shirt-zwXuzFr3mLs?utm_content=creditShareLink&utm_medium=referral&utm_source=unsplash";
 
 export default function Header() {
   const { user, logout, updateAvatar } = useAuthStore();
